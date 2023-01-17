39b9703d0d9fccad8af2e7c6be64eee2918356c4
LucaFaedda <lucakarl91@gmail.com>
2023-01-17 20:37:35 +0100

    millstone 0 done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
   export default {
+    components:{
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
       
   }
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
+    <AppFooter></AppFooter>
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @use './styles/general.scss' as *;
+  @use './styles/partials/mixin' as *;
+  @use './styles/partials/variabiles' as *;
+
 </style>
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    url: 'https://api.themoviedb.org/3/movie/550?api_key=692f0d50db196747e2b3f84e4902698b'
+})
